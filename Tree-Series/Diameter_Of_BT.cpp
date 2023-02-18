c0a936a4f4ee4fda2dec275b420cab73d7f07bd1
shubhamlahoti01 <lahotishubham03@gmail.com>
2023-02-18 23:06:26 +0530

    diameter of bt

---
+++ Tree-Series/Diameter_Of_BT.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/diameter-of-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int helper(TreeNode* root,int &d){
+        if(root==NULL) return 0;
+        int lh = helper(root->left,d);
+        int rh = helper(root->right,d);
+        d=max(d,lh+rh);
+        return 1+max(lh,rh);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(root==NULL) return 0;
+        int d=0;
+        helper(root,d);
+        return d;
+    }
+};
