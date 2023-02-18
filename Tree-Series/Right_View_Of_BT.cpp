944b9a90cbd296ae406ea4db01f7b7adad28e211
shubhamlahoti01 <lahotishubham03@gmail.com>
2023-02-19 00:50:35 +0530

    right view of bt

---
+++ Tree-Series/Right_View_Of_BT.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/binary-tree-right-side-view
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void helper(TreeNode* root, vector<int> &res, int l){
+        if(!root) return;
+        if(res.size()==l) res.push_back(root->val);
+        helper(root->right, res, l+1);
+        helper(root->left, res, l+1);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        helper(root, res, 0);
+        return res;
+    }
+};
